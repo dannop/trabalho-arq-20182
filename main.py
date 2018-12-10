c52b5ec64648e64f0d1b60dd1b2a8c8d5e775cd8
Daniel Lessa <daniellsf@id.uff.br>
2018-12-10 07:51:45 -0200

    Sistema de treinamento de uma rede neural

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+import network
+sys.path.insert(0, "/home/danno/Documentos/Dev/Python/Arq_trabalho/neural-networks-and-deep-learning/src")
+import mnist_loader
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
